3b0bb89b40119728fda1b692ec18412c7048576e
Teklitt <mebudenifemi@gmail.com>
2023-10-06 01:44:29 -0400

    adding responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,24 +19,24 @@ export default function Home() {
       </Head>
 
       <main className=" flex items-center text-dark w-full min-h-screen dark:text-light  ">
-        <Layout className="pt-0">
-          <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
+        <Layout className="pt-0 md:pt-16 sm:pt-8">
+          <div className="flex items-center justify-between w-full lg:flex-col">
+            <div className="w-1/2 md:w-full ">
               <Image
                 src={profilePic}
                 alt="femi "
-                className="w-full h-auto"
+                className="w-full h-auto lg:hidden md:inline-block md:w-full"
                 priority
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
 
-            <div className="w-1/2 flex flex-col items-center self-center">
+            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
                 text="Leadership in Tech - Empowering Innovation through Code."
-                className="!text-7xl !text-left"
+                className=" !text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-4xl sm:!text-2xl"
               />
-              <p className="my-4 text-base font-medium">
+              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
                 Hello, I'm Femi Mebude, a passionate and results-driven computer
                 scientist with a strong foundation in software engineering and a
                 flair for leadership.
@@ -50,11 +50,11 @@ export default function Home() {
                 creative problem-solving, and dedication to pushing the
                 boundaries of what's possible in technology. */}
               </p>
-              <div className="flex items-center self-start mt-2">
+              <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
                   href="/dummy.pdf"
                   target={'_blank'}
-                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light"
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
                   Resume <LinkArrow className="w-6 ml-2" />
@@ -62,7 +62,7 @@ export default function Home() {
                 <Link
                   href="mailto:mebudenifemi@gmail.com"
                   target={'_blank'}
-                  className="ml-4 flex items-center text-lg font-medium  underline dark:text-light"
+                  className="ml-4 flex items-center text-lg font-medium  underline dark:text-light md:text-base"
                 >
                   Contact
                 </Link>
@@ -71,7 +71,7 @@ export default function Home() {
           </div>
         </Layout>
         <HireMe />
-        <div className="absolute right-8 -bottom-20 inline-block w-40 ">
+        <div className="absolute right-8 -bottom-20 inline-block w-40 md:hidden ">
           <Image src={thinkingFace} alt="humour" className="w-full h-auto" />
         </div>
       </main>
