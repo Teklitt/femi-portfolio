2fc7b33b3c7017f18e9d97334a2230575fdba898
Teklitt <mebudenifemi@gmail.com>
2023-11-29 15:51:37 -0500

    Add Existing projects to Portfolio

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,7 +6,10 @@ import Link from 'next/link'
 import { GithubIcon } from '@/components/Icons'
 import Image from 'next/image'
 import project1 from '../../public/images/projects/AIprompts.png'
-import project2 from '../../public/images/projects/AIprompts.png'
+import flightPro from '../../public/images/projects/flightPro.png'
+import dxit from '../../public/images/projects/DXIT.png'
+import notion from '../../public/images/projects/Notion.png'
+import tfsupport from '../../public/images/projects/TF-Support.png'
 import project4 from '../../public/images/projects/profile Card.png'
 import { motion } from 'framer-motion'
 import TransitionEffect from '@/components/TransitionEffect'
@@ -150,37 +153,58 @@ const projects = () => {
                 img={project1}
                 summary="AI Prompt Share is an innovative full-stack app designed to ignite your creative spark. With features like user-friendly GoogleAuth login, personalized user profiles, and the ability to effortlessly create, save, and delete prompts, our platform is a hub for creative minds. Built using cutting-edge technologies like NextJS, TailwindCSS, MongoDB, and NextAuth."
                 link="https://aiprompts.teklittsolutions.com/"
-                type="Featured project"
+                type="Personal project"
                 github="https://github.com/Teklitt/Aiprompts"
+              />
+            </div>
+
+            <div className="col-span-12">
+              <FeaturedProject
+                title="TF Support Tools Page"
+                img={tfsupport}
+                summary="Developed a Support Page at Pluralsight using Typescript, Node.js, and React, streamlining common tasks for the TechFoundation support team at Pluralsight. By replacing the need for external tools like Postman, the project led to a remarkable 40% reduction in task completion time, translating to a 25% decrease in customer issue resolution time."
+                link="https://github.com/Teklitt/TF-Support-Tool/tree/main"
+                type="Personal project"
+                github="https://github.com/Teklitt/TF-Support-Tool/tree/main"
               />
             </div>
 
             <div className="col-span-6 sm:col-span-12">
               <Project
-                title="Ai PromptShare"
-                img={project2}
-                link="/"
-                type="Featured project"
-                github="https://github.com/Teklitt"
+                title="Profile Page"
+                img={project4}
+                link="https://github.com/Teklitt/profile_card"
+                type="Personal project"
+                github="https://github.com/Teklitt/profile_card"
               />
             </div>
             <div className="col-span-6 sm:col-span-12">
               <Project
-                title="Ai PromptShare"
-                img={project1}
-                link="/"
-                type="Featured project"
+                title="DXIT Roboot"
+                img={dxit}
+                link="https://github.com/Teklitt"
+                type="Personal project"
                 github="https://github.com/Teklitt"
               />
             </div>
             <div className="col-span-12">
               <FeaturedProject
-                title="Profile Card"
-                img={project4}
-                summary="Created a simple profile card and Navbar using Angular and Tailwind CSS"
-                link="https://github.com/Teklitt/profile_card"
-                type="Featured project"
-                github="https://github.com/Teklitt/profile_card"
+                title="Flight Pro"
+                img={flightPro}
+                summary="Flight Pro is an exclusive web application designed for AAdvantage members, offering unparalleled insights and convenience for moments when flight delays disrupt your plans. This app not only predicts potential flight delays but also empowers users with a comprehensive view of their available options in the event of disruptions"
+                link="https://github.com/Teklitt/Flight-Pro"
+                type="Team project"
+                github="https://github.com/Teklitt/Flight-Pro"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Notion API Integration"
+                img={notion}
+                summary="This program reads data from a CSV file (ratings.csv) containing book name, membername and ratings. Processes it to calculate average ratings for each book, favorite counts, and then populates a Notion database with the results."
+                link="https://www.notion.so/80abec0ce90546fda90db472fa1dbc40?v=946de1f64cc54b9f8e0fb96016365e2c"
+                type="Personal project"
+                github="https://github.com/Teklitt/Notion-Api-Integration"
               />
             </div>
           </div>
